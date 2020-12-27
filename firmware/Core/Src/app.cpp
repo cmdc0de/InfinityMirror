8625386fc174b1841170818611cbbaa3a4bf8e5f
cmdc0de <cmdc0dez@gmail.com>
2020-12-26 19:33:17 -0700

    working code with 5 different dances

--- firmware/Core/Src/app.cpp
+++ firmware/Core/Src/app.cpp
@@ -35,6 +35,7 @@ public:
 		return onRun(leds);
 	}
 	bool isDanceOver(uint32_t time) {return time>EndTime;}
+	uint32_t getStartTime() {return StartTime;}
 	virtual ~LedDance() {}
 protected:
 	virtual ErrorType onInit()=0;
@@ -48,30 +49,118 @@ private:
 
 class LedRandom : public LedDance {
 public:
-	LedRandom(uint32_t maxTime) : LedDance(), MaxTimeInMode(maxTime) {}
+	LedRandom(uint32_t maxTime, uint32_t timeBetweenChange) : LedDance(), MaxTimeInMode(maxTime), TimeBetweenChange(timeBetweenChange), LastTime(0) {}
 	virtual ~LedRandom() {}
 protected:
 	ErrorType onInit() override {
 		setTimeInMode(MaxTimeInMode);
+		LastTime = 0;
 		return ErrorType();
 	}
 	ErrorType onRun(cmdc0de::RGB leds[NUM_LEDS]) override {
-		for(int i=0;i<NUM_LEDS;++i) {
-			leds[i].setBrightness(rand()%31);
-			leds[i].setRed(rand()%255);
-			leds[i].setBlue(rand()%255);
-			leds[i].setBlue(rand()%255);
+		if((LastTime/TimeBetweenChange)!=(HAL_GetTick()/TimeBetweenChange)) {
+			LastTime = HAL_GetTick();
+			for(int i=0;i<NUM_LEDS;++i) {
+				leds[i].setBrightness(rand()%100);
+				leds[i].setRed(rand()%255);
+				leds[i].setBlue(rand()%255);
+				leds[i].setBlue(rand()%255);
+			}
 		}
 		return ErrorType();
 	}
 private:
 	uint32_t MaxTimeInMode;
+	uint32_t TimeBetweenChange;
+	uint32_t LastTime;
 };
-LedRandom TheLedRandom(10000);
-static const uint8_t TOTAL_MODES = 1;
-LedDance *DanceArray[TOTAL_MODES];
-bool up = false;
-uint8_t Mode = 0;
+
+class LedChase : public LedDance {
+public:
+	LedChase(uint32_t maxTime, uint32_t timeBetweenChange, bool bFill) : LedDance(), MaxTimeInMode(maxTime), TimeBetweenChange(timeBetweenChange), LastTime(0), Count(0), ChaseColor(), Fill(bFill) {}
+	virtual ~LedChase() {}
+protected:
+	ErrorType onInit() override {
+		setTimeInMode(MaxTimeInMode);
+		LastTime = 0;
+		ChaseColor = RGB(rand()%255,rand()%255,rand()%255,rand()%100);
+		return ErrorType();
+	}
+	ErrorType onRun(cmdc0de::RGB leds[NUM_LEDS]) override {
+		if((LastTime/TimeBetweenChange)!=(HAL_GetTick()/TimeBetweenChange)) {
+			LastTime = HAL_GetTick();
+			uint8_t startPos = Count;
+			for(int i=0;i<NUM_LEDS;i++) {
+				if((Fill && i< Count) || (!Fill &&i==Count)) {
+					leds[i] = ChaseColor;
+				} else {
+					leds[i].setBrightness(0);
+					leds[i].setBlue(0);
+					leds[i].setGreen(0);
+					leds[i].setRed(0);
+				}
+			}
+			Count = ++Count%NUM_LEDS;
+		}
+		return ErrorType();
+	}
+private:
+	uint32_t MaxTimeInMode;
+	uint32_t TimeBetweenChange;
+	uint32_t LastTime;
+	uint32_t Count;
+	RGB ChaseColor;
+	bool Fill;
+};
+
+class LedBreath : public LedDance {
+public:
+	LedBreath(uint32_t maxTime, uint32_t timeBetweenChange, bool bUp, uint8_t incBy)
+		: LedDance(), MaxTimeInMode(maxTime), TimeBetweenChange(timeBetweenChange), LastTime(0), ChaseColor(), Up(bUp), IncBy(incBy) {}
+	virtual ~LedBreath() {}
+protected:
+	ErrorType onInit() override {
+		setTimeInMode(MaxTimeInMode);
+		LastTime = 0;
+		ChaseColor = RGB(rand()%255,rand()%255,rand()%255,rand()%100);
+		return ErrorType();
+	}
+	ErrorType onRun(cmdc0de::RGB leds[NUM_LEDS]) override {
+		if((LastTime/TimeBetweenChange)!=(HAL_GetTick()/TimeBetweenChange)) {
+			LastTime = HAL_GetTick();
+			for(int i=0;i<NUM_LEDS;i++) {
+				int16_t bright = leds[i].getBrightness();
+				leds[i] = ChaseColor;
+				bright = Up ? bright+IncBy : bright-IncBy;
+				if(bright>=100) {
+					Up = false;
+					bright=100;
+				} else if (bright<=0) {
+					Up = true;
+					bright=0;
+				}
+				leds[i].setBrightness(bright);
+			}
+		}
+		return ErrorType();
+	}
+private:
+	uint32_t MaxTimeInMode;
+	uint32_t TimeBetweenChange;
+	uint32_t LastTime;
+	RGB ChaseColor;
+	bool Up;
+	uint8_t IncBy;
+};
+
+static LedRandom TheLedRandom(10000,500);
+static LedChase TheLedChase(10000,50, false);
+static LedChase TheLedChaseF(10000,50, true);
+static LedBreath TheLedBreath(10000,50,true,4);
+
+static const uint8_t TOTAL_MODES = 4;
+static LedDance *DanceArray[TOTAL_MODES];
+static uint8_t Mode = 0;
 
 
 void initApp() {
@@ -80,16 +169,23 @@ void initApp() {
   }
   apa102c.init(NUM_LEDS,&ledBuf[0]);
   DanceArray[0] = &TheLedRandom;
+  DanceArray[1] = &TheLedChase;
+  DanceArray[2] = &TheLedChaseF;
+  DanceArray[3] = &TheLedBreath;
+  Mode = rand()%TOTAL_MODES;
 }
 
 int8_t percent =100;
-uint16_t count = 0;
+//uint16_t count = 0;
+bool up = false;
+uint32_t lastTime = HAL_GetTick();
 
 void run() {
-//	uint32_t timeNow = HAL_GetTick();
-#if 1
+	uint32_t timeNow = HAL_GetTick();
+#if 0
 	HAL_Delay(10);
-	if(count==0) {
+	if((lastTime/30)!=(timeNow/30)) {
+		lastTime = timeNow;
 		apa102c.setBrightness(up ? percent++ : percent--);
 		apa102c.send();
 		if(percent<1) {
@@ -98,16 +194,14 @@ void run() {
 			up = false;
 		}
 	}
-	++count;
-	if(10000==count) count = 0;
+	//++count;
+	//if(10==count) count = 0;
 #else
 	if(DanceArray[Mode]->isDanceOver(timeNow)==true) {
-		Mode = ++Mode%TOTAL_MODES;
+		Mode = rand()%TOTAL_MODES;
 		DanceArray[Mode]->init();
 	}
 	DanceArray[Mode]->run(ledBuf);
-	apa102c.send();
-
-
+	apa102c.send(NUM_LEDS,&ledBuf[0]);
 #endif
 }
--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -26,7 +26,6 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "app.h"
-
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -99,9 +98,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
 	  run();
+    /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
@@ -156,7 +154,10 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-
+  __disable_irq();
+  while (1)
+  {
+  }
   /* USER CODE END Error_Handler_Debug */
 }
 
@@ -172,7 +173,7 @@ void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
